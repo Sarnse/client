93344d3acf55334eb710dd2625423b03b64ed72d
Johnnie Warren <Johnnieray_2002@yahoo.com>
2017-10-13 19:04:56 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponent
+    ContactsComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule
