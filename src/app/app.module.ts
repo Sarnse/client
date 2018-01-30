951fdd63c283705c02ed2c2978deb4326ef0b19b
Johnnie Warren <Johnnieray_2002@yahoo.com>
2018-01-30 18:45:20 -0500

    Second to last project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ClientComponent } from './client/client.component';
@@ -12,7 +12,7 @@ import { ClientComponent } from './client/client.component';
     ClientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
